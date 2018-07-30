0325ce194c188bae894362691ee3aecb688c7f2d
Dhruvan Ganesh <navrudh@gmail.com>
2018-07-30 17:28:52 +0530

    add components IssueList, IssuePage

--- src/AppMain/AppMain.tsx
+++ src/AppMain/AppMain.tsx
@@ -1,22 +1,15 @@
-import * as React from 'react';
-import logo from '../logo.svg';
 import './AppMain.css';
 
+import * as React from 'react';
+
+import IssuesPage from '../IssuesPage/IssuesPage';
+
 class AppMain extends React.Component {
-    public render() {
-        return (
-            <div className="App">
-              <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <h1 className="App-title">Welcome to React</h1>
-              </header>
-              <p className="App-intro">
-                To get started, edit <code>src/App.tsx</code> and save to reload.
-              </p>
-            </div>
-          );
-    }
+  public render() {
+    return (
+      <IssuesPage />
+    );
   }
-  
-  export default AppMain;
-  
+}
+
+export default AppMain;
---
+++ src/IssuesPage/IssuesList.tsx
@@ -0,0 +1,111 @@
+import * as React from 'react';
+
+import gql from "graphql-tag"
+import { Query } from "react-apollo";
+
+interface QueryResult {
+  repository: Repository;
+}
+
+interface Repository {
+  issues: Issues;
+}
+
+interface Issues {
+  totalCount: number;
+  edges: IssueEdges[];
+}
+
+interface IssueEdges {
+  node: IssueNode;
+}
+
+interface IssueNode {
+  id: string;
+  title: string;
+  url: string;
+  labels: IssueLabel;
+}
+
+interface IssueLabel {
+  edges: LabelEdges[];
+}
+
+interface LabelEdges {
+  node: LabelNode;
+}
+
+interface LabelNode {
+  name: string;
+  color: string;
+}
+
+const query = (repoOwner: string, repoName: string, issuesFirst: number, issueStatus: 'OPEN' | 'CLOSED') => gql`
+{
+  repository(owner: ${repoOwner}, name: ${repoName}) {
+    issues(first: ${issuesFirst}, states: ${issueStatus}) {
+      totalCount
+      edges {
+        node {
+          id
+          title
+          url
+          labels(first: 5) {
+            edges {
+              node {
+                name
+                color
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
+`
+
+interface IssuesQueryProps {
+  repoOwner: string,
+  repoName: string,
+  issuesFirst?: number,
+  issueStatus?: 'OPEN' | 'CLOSED'
+}
+
+class IssuesList extends React.Component<IssuesQueryProps> {
+
+  private repoOwner: string
+  private repoName: string
+  private issuesFirst: number
+  private issueStatus:  'OPEN' | 'CLOSED'
+
+  constructor(props: IssuesQueryProps) {
+    super(props);
+    props = props
+    this.repoOwner = props.repoOwner
+    this.repoName = props.repoName
+    this.issuesFirst = props.issuesFirst || 20
+    this.issueStatus = props.issueStatus || 'OPEN'
+  }
+
+  public render() {
+    return (
+      <Query query={query(this.repoOwner, this.repoName, this.issuesFirst, this.issueStatus)}>
+        {({ loading, error, data }) => {
+          if (loading) { return <p>Loading...</p>; }
+          if (error) { return <p>Error :(</p>; }
+
+          const queryResult: QueryResult = data
+          return queryResult.repository.issues.edges.map(({ node }) => (
+            <div key={node.id}>
+              <p>{`${node.title}: ${node.url}`}</p>
+            </div>
+          ));
+        }}
+      </Query>
+    );
+  }
+}
+
+export default IssuesList;
